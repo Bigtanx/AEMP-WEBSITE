2cc77afe1bce65a546065c27afe2fc7ca88126c6
Olubanji tobi <doluwatobiloba73@gmail.com>
2025-08-05 14:21:54 +0100

    updated full color for the uie

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./public/**/*.html", "./public/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightGreen: "#00A859", // 🌿 Brand Green
+        brightOrange: "#FF6A00", // 🟧 Call-to-Action Orange
+        lightGreenTint: "#e6f4ec", // 🍃 Background Accent
+      },
+    },
   },
   plugins: [],
 };
